dfce82e12186a230ebb46966807881ed5946b526
nasi <nasigandul@yahoo.com>
2017-07-21 14:14:02 +0000

    edit gitignore admin course task

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,6 +5,6 @@ class Course < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true, numericality: { only_integer: true}
   
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { medium: "680x300>", thumb: "170x75>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/  
 end
